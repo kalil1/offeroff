2c1ab1ed3d3f0845261a0d2ba2e53cf7020c7ad7
kalil <wkalil416@gmail.com>
2019-01-11 21:12:41 -0500

    pics

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,4 @@ class Product < ApplicationRecord
   has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
 
-  validates_attachment_size :product_img, :less_than => 5.megabytes
-  validates_attachment_content_type :product_img, :content_type => ['image/jpeg', 'image/png','image/gif','image/jpg']
 end
