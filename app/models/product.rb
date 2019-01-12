0bf9ec18dd68b5ae8595dfd80b5b485719378085
kalil <wkalil416@gmail.com>
2019-01-12 12:02:07 -0500

    forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, url: "system/photo_imgs/:id/:style/:basename.:extension", path: "#{Rails.root}/public/system/photo_imgs/:id/:style/:basename.:extension"
-  validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
-
+  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_size :product_img, :less_than => 5.megabytes
+  validates_attachment_content_type :product_img, :content_type => ['image/jpeg', 'image/png','image/gif','image/jpep']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_01_153918) do
+ActiveRecord::Schema.define(version: 2018_03_28_021239) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
