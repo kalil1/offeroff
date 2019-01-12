0c89a96b1702207457a63bd1575b44a80f119279
kalil <wkalil416@gmail.com>
2019-01-11 21:23:19 -0500

    pics

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, url: "system/photo_imgs/:id/:style/:basename.:extension", path: "#{Rails.root}/public/system/photo_imgs/:id/:style/:basename.:extension"
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
 
 end
