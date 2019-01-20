f338f541df65c877634a8bb95b7d5d251d648519
kalil <wkalil416@gmail.com>
2019-01-19 23:58:40 -0500

    upload logic

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :product_img, styles: { product_index: "300x300>", product_show: "400x250>" }, default_url: "/images/:style/missing.png"
   validates_attachment_size :product_img, :less_than => 5.megabytes
   validates_attachment_content_type :product_img, :content_type => ['image/jpeg', 'image/png','image/gif','image/jpep']
 end
