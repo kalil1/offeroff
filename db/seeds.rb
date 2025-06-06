7e8da666c91310a32b423acc5e86716fa574419e
kalil1 <wkalil77@gmail.com>
2025-06-05 22:01:40 -0400

    update: new seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-categories = [ 'Clothing', 'Tech', 'Sports', 'Cars', 'Hardware', 'Free']
-categories.each do |cate|
-Category.create(:name => cate)
+# Clear existing data (optional, use with caution)
+Category.destroy_all
+User.destroy_all
+Product.destroy_all
+
+# Seed Categories
+categories = ['Clothing', 'Tech', 'Sports', 'Cars', 'Hardware', 'Free']
+categories.each { |cate| Category.create!(name: cate) }
+puts "Created #{Category.count} categories"
+
+# Seed Users
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: 'password123',
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    username: Faker::Internet.unique.username
+  )
 end
+puts "Created #{User.count} users"
+
+# Seed Products
+1000.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    price: Faker::Commerce.price(range: 1.0..500.0),
+    user: User.order("RANDOM()").first,
+    category: Category.order("RANDOM()").first
+  )
+end
+puts "#{Product.count} Created #{Product.name} product"
 
 p 'Done'
